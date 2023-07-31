650520b9839d5f2fdc1c0be36ecb3f05c83709c6
saudkhan <saudkhanbpk@gmail.com>
2023-07-31 14:44:50 +0500

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function Header() {
+  return (
+    <>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container fluid>
+          <Navbar.Brand href="#">Logo</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+              <Nav.Link href="#action1">Campaigns</Nav.Link>
+              <Nav.Link href="#action2">Influencers</Nav.Link>
+              <Nav.Link href="#">Influential Fans</Nav.Link>
+              <Nav.Link href="#">Assets</Nav.Link>
+              <Nav.Link href="#">Free tools</Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <div className="credits__button mx-3">Credits remaining: 100</div>
+              <div><i className="fa-solid fa-life-ring fa-lg  mx-3"></i></div>
+              <div><i className="fa-solid fa-bell fa-lg  mx-3"></i></div>
+              <div><i className="fa-solid fa-user fa-lg  mx-3"></i></div>
+
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  )
+}
+
+export default Header
---
+++ src/components/pages/Home.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import Layout from '../Layout'
+import { InstagramLogin } from "@amraneze/react-instagram-login";
+import instagram from './../../assets/1658587303instagram-png.png'
+function Home() {
+
+  const responseInstagram = (response) => {
+    console.warn(response);
+  };
+
+
+  const clientId = "827238338930101";
+  const redirectUrl = "https://imrankhan44-portfolio.netlify.app/";
+  return (
+    <Layout>
+      <div className="d-flex flex-column justify-content-center align-items-center mt-5">
+        <img src={instagram} alt="" className='home-insta__image' />
+        <div className='mt-2'>
+          <h1 className='text-center mb-2'>Instagram Mentions Tracking  </h1>
+          <h2>Automatically Collect Posts & Stories your brand gets mentioned in</h2>
+        </div>
+      </div>
+      <div className="d-flex flex-column justify-content-center align-items-center mt-3">
+        <InstagramLogin
+          clientId={clientId}
+          onSuccess={responseInstagram}
+          onFailure={responseInstagram}
+          redirectUri={redirectUrl}
+          scope="user_profile,user_media,publish_video,pages_show_list,instagram_basic,instagram_content_publish,pages_read_engagement"
+          useRedirect={false}
+        >
+          <i className="fab fa-instagram fa-lg"></i>
+          <span> Login with Instagram</span>
+        </InstagramLogin>
+        <div className='mt-4'>
+          <p>please Note: You will be redirected to facebook where you can select the instagram business profile</p>
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export default Home
